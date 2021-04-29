28dfb31f5255e78763d13f4dd40c377cd25a8d74
rivegav <79854144+rivegav@users.noreply.github.com>
2021-04-28 22:24:54 -0500

    Parte 1 vehiculo

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,33 +1,53 @@
 
-/**
- * Write a description of class Vehiculo here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Vehiculo
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Vehiculo
-     */
-    public Vehiculo()
-    {
-        // initialise instance variables
-        x = 0;
+    public static Vehiculo[][] vehiculo;
+    public static int size = 0;
+    private String brand;
+    private String color;
+    private String placa;
+    private int value;
+    
+    public Vehiculo(){
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public Vehiculo(String p, String b, String c, int v){
+        this(p, b, c, 30000000);
     }
+    
+    public Vehiculo(String p, String b, String c, int v){
+        size++;
+        this.placa = p;
+        this.brand = b;
+        this.color = c;
+        this.value = v;
+    }
+    public void setPlaca(String p){
+        this.placa = p;
+    }
+    public void setBrand(String b){
+        this.brand = b;
+    } 
+    public void setColor(String c){
+        this.Color = c;
+    }
+    public void setValue(int v){
+        this.value = v;
+    }
+    public String getPlaca(){
+        return this.placa;
+    }
+    public String getBrand(){
+        return this.brand;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    public int getValue(){
+        return this.value; 
+    }
+    public static int getsize(){
+        return s; 
+    }
+        
 }
