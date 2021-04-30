e4d263d1eb3311b0b349fd77415f552c89d53305
rivegav <79854144+rivegav@users.noreply.github.com>
2021-04-30 17:08:36 -0500

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -38,15 +38,111 @@ public class Principal{
         System.out.println("Pulse 8, para filtar los vehiculos por color");
         System.out.println("Pulse 9, para mostrar vehiculos por valor comercial");
         int num = scan.nextInt();
-        /**
-         * Not finished faltan los casos y la repeticion del menu
-         * Faltan multiples variables
-         */
+        
+        while(num!=0){
+             case 1:
+                            System.out.println(Sensor.sensorLibre());
+                            break;
+                        case 2:
+                            System.out.println("Ingrese el piso y el espacio que desea parquear su vehiculo en: ");
+                            int piso=scan.nextInt();
+                            int espacio= scan.nextInt();
+                            if(Vehiculo.cantidad<=Vehiculo.getTamaño()){
+                                if(Sensor.sensores[piso][espacio].getEstado()==0){
+                                    System.out.println("Ingrese por favor placa, marca y color");
+                                    String placa = scan.next();
+                                    String marca = scan.next();
+                                    String color = scan.next();
+                                    
+                                    Vehiculo.vehiculo[piso][espacio] = new Vehiculo(placa, marca,color);
+                                    Sensor.sensores[piso][espacio].setEstado(1);
+                                    
+                                    System.out.println(Vehiculo.vehiculo[piso][espacio]);
+                                } else {
+                                    System.out.println("El lugar está ocupado. Por favor elija otro espacio.");
+                                }
+                            } else {
+                                System.out.println("El estacionamiento está lleno.");                        
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Ingrese el piso y el espacio que desea parquear su vehiculo en: ");                         
+                            piso = scan.nextInt();
+                            espacio = scan.nextInt();
+                                
+                            if(Vehiculo.cantidad<=Vehiculo.getTamaño()){
+                                 if(Sensor.sensores[piso][espacio].getEstado()==0){
+                                      System.out.println("Ingrese por favor placa, marca, color y valor comercial del vehículo.");
+                                      String placa = scan.next();
+                                      String marca = scan.next();
+                                      String color = scan.next();
+                                      int valor = scan.nextInt();
+                                        
+                                      Vehiculo.vehiculo[piso][espacio]= new Vehiculo (placa, marca,color,valor);
+                                      Sensor.sensores[piso][espacio].setEstado(1);
+                                        
+                                      System.out.println(Vehiculo.vehiculo[piso][espacio]);
+                                } else {
+                                    System.out.println("Lugar Ocupado. Por favor elija otro espacio.");
+                                    }
+                                } else {
+                                    System.out.println("El estacionamiento está lleno.");
+                            }
+                            break;
+                        case 4:
+                            System.out.println(Vehiculo.toStringVehiculos());
+                            break;
+                        case 5:
+                            System.out.println("Hay " +Vehiculo.cantidad+ " vehículo(s) estacionado(s).");
+                            break;
+                        case 6:
+                            System.out.println("Ingrese el piso y el espacio que desea verificar: ");
+                            piso=scan.nextInt();
+                            espacio= scan.nextInt();
+                            if(Sensor.sensores[piso][espacio].getEstado()==0){
+                                System.out.println("Estado: "+Sensor.sensores[piso][espacio].getEstado()+" (LIBRE)");
+                            } else {
+                                System.out.println("Estado: "+Sensor.sensores[piso][espacio].getEstado()+" (OCUPADO)");
+                            }
+                            break;
+                        case 7:
+                            System.out.println(Sensor.sensoresEstado());
+                            break;
+                            
+                        case 8:
+                            System.out.println("Ingrese el color a mostrar:");
+                            String color = scan.next();
+                            System.out.println(Vehiculo.vehiculosColor(color));
+                            break;
+                        case 9:
+                            Vehiculo[] arr = Vehiculo.ordenadosValor(Vehiculo.vehiculo);
+                            for(int i=0;i<Vehiculo.cantidad;i++){
+                                System.out.println(arr[i]);
+                            }
+                            break;
+                        default:
+                            System.out.println("Comando inválido. Por favor intente de nuevo");
+                            break;
+            }
+                      System.out.println("Escriba una opción: ");
+                      System.out.println("0. Salir del sitema.");
+                      System.out.println("1. Sensores libres.");
+                      System.out.println("2. Registrar parqueo de vehículo.");
+                      System.out.println("3. Registrar parqueo de vehículo con valor comercial.");
+                      System.out.println("4. Listar información vehículos que están parqueados.");
+                      System.out.println("5. Mostrar cantidad vehículos que se han parqueado.");
+                      System.out.println("6. Verificar estado de un lugar en el estacionamiento.");
+                      System.out.println("7. Mostrar estado de sensores.");
+                      System.out.println("8. Filtrar por color."); 
+                      System.out.println("9. Mostrar vehículos por valor comercial.");
+                      num = scan.nextInt();
+        }
+                System.out.println("Cerrando terminal...");
     }
 }
-                
-        
-        
+      
+                  
+
         
        
        
