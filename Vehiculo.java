81ddd0ac5e81fe56386019bee02ab0ff5f1493f3
rivegav <79854144+rivegav@users.noreply.github.com>
2021-04-30 17:18:06 -0500

    Update Vehiculo.java

--- Vehiculo.java
+++ Vehiculo.java
@@ -2,7 +2,8 @@
 public class Vehiculo
 {
     public static Vehiculo[][] vehiculo;
-    public static int size = 0;
+    public static int size;
+    public static int quantity = 0;
     private String brand;
     private String color;
     private String placa;
@@ -49,5 +50,63 @@ public class Vehiculo
     public static int getsize(){
         return s; 
     }
-        
+    
+     public String toString(){
+             return "Placa: " + this.placa +". Marca: " +this.brand+ ". Color: " +this.color+ ". Valor: "+ this.value;
+         }
+     
+         public static String toStringVehiculos(){ 
+             String h="";
+             for (int i=0;i<vehiculo.length ;i++) {
+                 for(int j=0;j<vehiculo[i].length;j++) {
+                     if(vehiculo[i][j]!=null){
+                         h+="Placa: "+ vehiculo[i][j].placa+ ". Marca: "+vehiculo[i][j].brand+ ". Color: "+vehiculo[i][j].color+ ". Valor: " +vehiculo[i][j].value+"\n";
+                        }else{
+                         continue;
+                     }
+                }
+            }
+             return h;
+             }
+         public static int quantityVehiculos(){
+             return quantity;
+         }
+         public static String vehiculosColor(String c){
+             String text="";
+             
+             for(int i=0; i< vehiculo.length;i++){
+                 for(int j=0;j<vehiculo[i].length;j++) {
+                     if((Sensor.sensores[i][j].getEstado()==1)&&(vehiculo[i][j].color.equalsIgnoreCase(c))){
+                         text+=vehiculo[i][j].toString()+". Piso: "+i+". Espacio: "+j+"\n";
+                     }
+                 }
+             }
+             return text;
+         }
+         public static Vehiculo[] ordenadosValor(Vehiculo[][] ref){
+             Vehiculo[]A = new Vehiculo[Vehiculo.quantity];
+             Vehiculo aux = new Vehiculo();
+             int c=0;
+             for (int i=0; i<ref.length;i++) {
+                 for(int j=0;j<ref[0].length;j++) {
+                     if(Vehiculo.vehiculo[i][j]!=null) {
+                         A[c] = ref[i][j];
+                         c++;
+                     }                     
+                 }         
+             }
+             for (int i = 0; i < A.length - 1; i++) {
+                 for (int j = 0; j < A.length - i - 1; j++) {
+                     if (A[j + 1].getValue() < A[j].getValue()) {
+                         aux = A[j + 1];
+                         A[j + 1] = A[j];
+                         A[j] = aux;
+                        }
+                    }
+                }
+             
+             return A;
+         }
+    }
+
 }
